559ba0321e99c9ea636b38fa135bcd079100f4c4
Alex Makarov <mackarowsa@gmail.com>
2023-12-01 17:30:45 +0300

    make auth, model user

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,16 +1,28 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import TitleViewSet, GenreViewSet, CategoryViewSet
+from .views import (
+    UserRegistrationView, UserTokenView, UserViewSet,
+    TitleViewSet, GenreViewSet, CategoryViewSet,
+)
 
 
 router = DefaultRouter()
-router.register(r'titles', TitleViewSet)
-router.register(r'genres', GenreViewSet)
-router.register(r'categories', CategoryViewSet)
+router.register(r'users', UserViewSet, basename='users')
+router.register(r'titles', TitleViewSet, basename='titles')
+router.register(r'genres', GenreViewSet, basename='genres')
+router.register(r'categories', CategoryViewSet, basename='categories')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path(
+        'v1/auth/signup/',
+        UserRegistrationView.as_view(),
+        name='registration'
+    ),
+    path(
+        'v1/auth/token/',
+        UserTokenView.as_view(),
+        name='token'
+    ),
 ]
-
-
---
+++ api_yamdb/constants.py
@@ -0,0 +1,6 @@
+USER = 'user'
+MODERATOR = 'moderator'
+ADMIN = 'admin'
+
+LIFE_CONFIRMATION_CODE: int = 300
+LENGTH_CODE: int = 6
