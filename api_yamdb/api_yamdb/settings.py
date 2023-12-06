5942ec8aa6a629288456811ee95608cfc17c62c8
Alex Makarov <mackarowsa@gmail.com>
2023-12-06 12:30:33 +0300

    fixed bug

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -142,14 +142,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
 
-EMAIL_HOST = os.environ['EMAIL_HOST_USER']
+# EMAIL_HOST = os.environ['EMAIL_HOST_USER']
 
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+# EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+# EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
-EMAIL_PORT = 587
+# EMAIL_PORT = 587
 
-EMAIL_USE_SSL = False
+# EMAIL_USE_SSL = False
 
-EMAIL_USE_TLS = True
+# EMAIL_USE_TLS = True
